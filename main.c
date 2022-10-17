13d1268ca642d828776f48f4ab080864d5258597
abdalalkaissi11 <114093022+AbdalAlkaissi@users.noreply.github.com>
2022-10-17 10:43:46 -0500

    Update main.c

--- main.c
+++ main.c
@@ -2,14 +2,15 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void power();
-void reverse();
-void replace();
-void palindrome();
+void power(int _num);
+void reverse(int _num);
+void replace(int _num);
+void palindrome(int _num);
 
 void main()
 {
-    int num = 0;
+    int num;
+    int _num;
 
     printf("Enter the menu option for the operation to perform: ");
     printf("\n(1) POWER OF 2");
@@ -20,27 +21,34 @@ void main()
 
     scanf("%d", &num);
 
-
-    if (num == 1)
+    if ((num == 1) || (num == 2) || (num == 3) || (num == 4))
     {
-        power();
-    }
-    else if (num == 2)
-    {
-        reverse();
-    }
-    else if (num == 3)
-    {
-        replace();
-    }
-    else if (num == 4)
-    {
-        palindrome();
+        while (1)
+        {
+            printf("Enter a positive integer less than 2 billion: ");
+            scanf("%d", &_num);
+            if (_num > 0 && _num < 2000000000)
+                break;
+        }
+        if (num == 1)
+        {
+            power(_num);
+        }
+        else if (num == 2)
+        {
+            reverse(_num);
+        }
+        else if (num == 3)
+        {
+            replace(_num);
+        }
+        else if (num == 4)
+        {
+            palindrome(_num);
+        }
     }
     else if (num == 5)
-    {
         exit;
-    }
     else
     {
         printf("Error: Invalid option. Please try again.\n");
@@ -48,115 +56,77 @@ void main()
         main();
     }
 }
-void power()
+
+void power(int _num)
 {
-        int num;
-        int count = 0;
-        int power = 0;
-        int next = 0;
+    int count = 0;
+    int power = 0;
+    int next = 0;
 
-
-        while (true)
+    for (int i = 0; i < 32; i++)
+    {
+        if (_num & (1 << i))
         {
-            printf("Enter a positive integer less than 2 billion: ");
-            scanf("%d", &num);
-            if (num > 0 && num < 2000000000)
-                break;
+            count++;
         }
-
-        for (int i = 0; i < 32; i++)
-        {
-            if (num & (1 << i))
-            {
-                count++;
-            }
-        }
-
-        if (count == 1)
-        {
-            printf("\n%d is a power of 2.\n", num);
-        }
-        else
-        {
-            printf("\n%d is not a power of 2.\n", num);
-
-            for (int j = 0; j < 32; j++)
-            {
-                if (num & (1 << j))
-                {
-                    power = j;
-                }
-            }
-
-            next = 1 << (power + 1);
-
-            printf("\nNext higher integer that is power of 2 is: %d.\n", next);
-        }
-    main();
-}
-
-void reverse()
-{  
-// Function to reverse bits of num
-unsigned int reverseBits(unsigned int x)
-{
-    //Declare size
-    unsigned int numOfBits = sizeof(x) * 8;
-    
-    //Initialize revereNum and i
-    unsigned int reverseNum = 0;
-    int i;
-    
-    //Reverse num using bitwise
-    for (i = 0; i < numOfBits; i++) {
-        if ((x & (1 << i)))
-            reverseNum |= 1 << ((numOfBits - 1) - i);
     }
-    //Return operation as reverseNum
-    return reverseNum;
-}
 
-int main()
-{
-    unsigned int num;
-    //Ask for and take input a decimal
-    printf("Enter a positive integer less than 2 billion: ");
-    scanf("%u", &num);
-    
-  //Test to make sure its greater than 0 and less then 2,000,000,000
-    while(num < 0 || num > 2000000000){
-      printf("Enter a positive integer less than 2 billion: ");
-    scanf("%u", &num);
-  }  
-    //Print output
-    printf("%u with bits reversed is %u",num, reverseBits(num));
-    getchar();
-}
+    if (count == 1)
+    {
+        printf("\n%d is a power of 2.\n", _num);
+    }
+    else
+    {
+        printf("\n%d is not a power of 2.\n", _num);
+
+        for (int j = 0; j < 32; j++)
+        {
+            if (_num & (1 << j))
+            {
+                power = j;
+            }
+        }
+
+        next = 1 << (power + 1);
+
+        printf("\nNext higher integer that is power of 2 is: %d.\n", next);
+    }
     main();
 }
 
-void replace()
+void reverse(int _num)
 {
-    int integer;
+    unsigned int _num1 = _num;
+        //Declare size
+        unsigned int numOfBits = sizeof(_num) * 8;
+
+        //Initialize revereNum and i
+        unsigned int reverseNum = 0;
+        int i;
+
+        //Reverse num using bitwise
+        for (i = 0; i < numOfBits; i++) {
+            if ((_num1 & (1 << i)))
+                reverseNum |= 1 << ((numOfBits - 1) - i);
+        }
+
+        printf("%u with bits reversed is %u", _num1, reverseNum);
+        main();
+    }
+
+void replace(int _num)
+{
     int integerMask;
     int bitRepPos;
 
-
-    while (true)
-    {
-        printf("Enter a positive integer less than 2 billion: ");
-        scanf("%d", &integer);
-        if (integer > 0 && integer < 2000000000)
-            break;
-    }
-    while (true)
+    while (1)
     {
         printf("Enter a positive integer mask less than 3 billion: ");
         scanf("%d", &integerMask);
         if (integerMask > 0 && integerMask < 3000000000)
             break;
     }
-    while (true)
+    while (1)
     {
         printf("Enter the bit replacement position from mask (0-indexed): ");
         scanf("%d", &bitRepPos);
@@ -173,39 +143,31 @@ void replace()
         unsigned int temp = 4294967295; //set all 32 digits to 1
         digit = 1 << bitRepPos; //make a number, all of its bits are 0 except the postion you need to replace
         temp = temp ^ digit; //use XOR between temp and digit to make temp be all ones except the bit replacement postion which will be 0
-        integer = integer & temp; //use AND 
+        _num = _num & temp; //use AND 
         //printf("New integer after replacement is: %d", integer); 
     }
     else //(digit is 1) use OR
     {
         digit = digit << bitRepPos; //shift digit by bitreppos
-        integer = integer | digit; //use OR to replace
+        _num = _num | digit; //use OR to replace
 
     }
-    printf("New integer with bit %d from mask %d is %d\n", bitRepPos, integerMask, integer);
+    printf("New integer with bit %d from mask %d is %d\n", bitRepPos, integerMask, _num);
     main();
 }
 
-void palindrome()
+void palindrome(int _num)
 {
     #define SIZE 32
-    unsigned int n;
-    while (1)
-    {
-        printf("Enter a positive integer less than 2 billion: ");
-        scanf("%d", &n);
-        if (n > 0 && n < 2000000000)
-            break;
-    }
 
     //fill array with zeros
     int a[SIZE] = { 0 };
 
     int i = SIZE - 1;
-    while (n != 0) {
-        a[i--] = n & 1;
+    while (_num != 0) {
+        a[i--] = _num & 1;
         //right shift 
-        n = n >> 1;
+        _num = _num >> 1;
     }
     printf("Binary representation : ");
 
