6ff76b78b2ce41b6511fa196e34ff49156d41b0c
abdalalkaissi11 <114093022+AbdalAlkaissi@users.noreply.github.com>
2022-10-19 10:01:07 -0500

    Update replace.c

--- replace.c
+++ replace.c
@@ -1,50 +1,41 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-int main() {
-	int integer;
-	int integerMask;
-	int bitRepPos;
+void replace(int _num)
+{
+    int integerMask;
+    int bitRepPos;
 
+    while (1)
+    {
+        printf("Enter a positive integer mask less than 3 billion: ");
+        scanf("%d", &integerMask);
+        if (integerMask > 0 && integerMask < 3000000000)
+            break;
+    }
+    while (1)
+    {
+        printf("Enter the bit replacement position from mask (0-indexed): ");
+        scanf("%d", &bitRepPos);
+        if (bitRepPos >= 0 && bitRepPos <= 31)
+            break;
+    }
 
-	while (true)
-	{
-		printf("Enter a positive integer less than 2 billion: ");
-		scanf("%d", &integer);
-		if (integer > 0 && integer < 2000000000)
-			break;
-	}
-	while (true)
-	{
-		printf("Enter a positive integer mask less than 3 billion: ");
-		scanf("%d", &integerMask);
-		if (integerMask > 0 && integerMask < 3000000000)
-			break;
-	}
-	while (true)
-	{
-		printf("Enter the bit replacement position from mask (0-indexed): ");
-		scanf("%d", &bitRepPos);
-		if (bitRepPos >= 0 && bitRepPos <= 31)
-			break;
-	}
+    //shift the integer right by the bit replacement position to make it be on the far right and use bitwise operator AND with 1, to clear all the other digits on its left and keep it alone
+    int digit = (integerMask >> bitRepPos) & 1;
 
-	//shift the integer right by the bit replacement position to make it be on the far right and use bitwise operator AND with 1, to clear all the other digits on its left and keep it alone
-	int digit = (integerMask >> bitRepPos) & 1;
+    //2 scenarios, either the digit is 1 or 0
+    if (digit == 0) //use AND
+    {
+        unsigned int temp = 4294967295; //set all 32 digits to 1
+        digit = 1 << bitRepPos; //make a number, all of its bits are 0 except the postion you need to replace
+        temp = temp ^ digit; //use XOR between temp and digit to make temp be all ones except the bit replacement postion which will be 0
+        _num = _num & temp; //use AND 
+        //printf("New integer after replacement is: %d", integer); 
+    }
+    else //(digit is 1) use OR
+    {
+        digit = digit << bitRepPos; //shift digit by bitreppos
+        _num = _num | digit; //use OR to replace
 
-		//2 scenarios, either the digit is 1 or 0
-		if (digit == 0) //use AND
-		{
-		unsigned int temp = 4294967295; //set all 32 digits to 1
-		digit = 1 << bitRepPos; //make a number, all of its bits are 0 except the postion you need to replace
-		temp = temp ^ digit; //use XOR between temp and digit to make temp be all ones except the bit replacement postion which will be 0
-		integer = integer & temp; //use AND 
-		//printf("New integer after replacement is: %d", integer); 
-		}
-		else //(digit is 1) use OR
-		{
-			digit = digit << bitRepPos; //shift digit by bitreppos
-			integer = integer | digit; //use OR to replace
-
-		}
-		return integer;
+    }
+    printf("New integer with bit %d from mask %d is %d\n", bitRepPos, integerMask, _num);
+    main();
 }
