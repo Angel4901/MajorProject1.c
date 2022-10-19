275ce947c7afa23dd9bdb750e32b3d1b5a9f0990
abdalalkaissi11 <114093022+AbdalAlkaissi@users.noreply.github.com>
2022-10-19 10:14:05 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,3 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<stdlib.h>
-
-void power(int _num);
-void reverse(int _num);
-void replace(int _num);
-void palindrome(int _num);
-
 void main()
 {
     int num;
