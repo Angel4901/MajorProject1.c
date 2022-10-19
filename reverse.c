e038f7c09b7cc4d4271d4fa66405c160815cdaf7
Angel4901 <113717724+Angel4901@users.noreply.github.com>
2022-10-19 09:56:14 -0500

    Update reverse.c

--- reverse.c
+++ reverse.c
@@ -1,1 +1,19 @@
+void reverse(int _num)
+{
+    unsigned int _num1 = _num;
+        //Declare size
+        unsigned int numOfBits = sizeof(_num) * 8;
 
+        //Initialize revereNum and i
+        unsigned int reverseNum = 0;
+        int i;
+
+        //Reverse num using bitwise
+        for (i = 0; i < numOfBits; i++) {
+            if ((_num1 & (1 << i)))
+                reverseNum |= 1 << ((numOfBits - 1) - i);
+        }
+
+        printf("%u with bits reversed is %u", _num1, reverseNum);
+        main();
+    }
