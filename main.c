cd0562ce628456c6af628eb72c0f462f9c0881c1
rand199 <113040330+rand199@users.noreply.github.com>
2022-10-19 10:02:04 -0500

    Update main.c

--- main.c
+++ main.c
@@ -115,35 +115,3 @@ void reverse(int _num)
     }
 
 
-void palindrome(int _num)
-{
-    unsigned int no = _num;
-
-    int array[32] = { 0 };
-        int i = 32 - 1;
-        while (no != 0) {
-            array[i--] = no & 1;
-            no = no >> 1;
-        }
-        printf("Binary representation : ");
-
-        for (int r = 0;r < 32;r++)
-            printf("%d", array[r]);
-        printf("\n");
-
-        int test = 1;
-        for (int r = 0, no = 32 - 1;r < no;r++, no--) {
-            
-            if (array[r] != array[no]) {
-                test = 0;
-            }
-        }
-        if (test == 0) {
-            printf("is not a palindrome\n");
-        }
-        else {
-            printf("it is a palindrome\n");
-        }
-
-        main();
-    }
